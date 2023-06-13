efe601f515f44215c06e1e5ad249700421364c01
Zagros Jawar <135693273+zagrosjawar@users.noreply.github.com>
2023-06-13 23:43:32 +0200

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,16 @@
+def factorial(n):
+    """
+    Calculate the factorial of a number using recursion.
+    
+    Args:
+        n (int): The number to calculate the factorial of.
+        
+    Returns:
+        int: The factorial of the given number.
+    """
+    # Base case: factorial of 0 is 1
+    if n == 0:
+        return 1
+    
+    # Recursive case: factorial of n is n multiplied by factorial of (n-1)
+    return n * factorial(n - 1)
